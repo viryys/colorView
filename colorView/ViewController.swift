b0bfde55b044f6441c1384ed83dd650d4435f7ca
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2020-10-19 10:12:20 +0500

    финальная версия домашку

--- colorView/ViewController.swift
+++ colorView/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorViewController: UIViewController {
 
+    @IBOutlet weak var backgroundChangeView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        backgroundChangeView.layer.cornerRadius = 10
+        
+        redValueLabel.text = String(Int(redSlider.value.rounded()))
+        greenValueLabel.text = String(Int(greenSlider.value.rounded()))
+        blueValueLabel.text = String(Int(greenSlider.value.rounded()))
+        backgroundChangeView.backgroundColor = self.setBackground()
+    }
+    
+    func setBackground() -> UIColor {
+        return UIColor(
+            red: CGFloat(round(redSlider.value)/255.0),
+            green: CGFloat(round(greenSlider.value)/255.0),
+            blue: CGFloat(round(blueSlider.value)/255.0),
+            alpha: 1.0)
     }
 
-
+    @IBAction func changeRedColor() {
+        redValueLabel.text = String(Int(redSlider.value.rounded()))
+        backgroundChangeView.backgroundColor = self.setBackground()
+    }
+    @IBAction func changeGreenColor() {
+        greenValueLabel.text = String(Int(greenSlider.value.rounded()))
+        backgroundChangeView.backgroundColor = self.setBackground()
+    }
+    @IBAction func changeBlueColor() {
+        blueValueLabel.text = String(Int(blueSlider.value.rounded()))
+        backgroundChangeView.backgroundColor = self.setBackground()
+    }
+    
 }
 
